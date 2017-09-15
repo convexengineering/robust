42cd4889651a44006eaa48f3b07e2db9b42c25c2
Ali Saab <alikassemsaab@gmail.com>
2017-09-15 18:54:03 -0400

    a file for Ned to check

--- Ned.py
+++ Ned.py
@@ -6,43 +6,43 @@ import GPModels as models
 
 tm = models.test_model()
 
-tm_box_uncertain_coeff = RobustGPModel(tm, 0, 'box')
+tm_box_uncertain_coeff = RobustGPModel(tm, 1, 'box')
 # tm_box_uncertain_coeff.setup() creates the robust model and saves it in tm_box_uncertain_coeff.robust_model
-sol_tm_box_uncertain_coeff = tm_box_uncertain_coeff.solve(verbosity=0)  # calls the setup() if the robust model is not created yet,
+sol_tm_box_uncertain_coeff = tm_box_uncertain_coeff.solve(verbosity=1)  # calls the setup() if the robust model is not created yet,
 # otherwise it will directly solve the robust_model
 tm_box_uncertain_coeff_robust_model = tm_box_uncertain_coeff.robust_model
 
-tm_ell_uncertain_coeff = RobustGPModel(tm, 0, 'elliptical')
-sol_tm_ell_uncertain_coeff = tm_ell_uncertain_coeff.solve(verbosity=0)
+tm_ell_uncertain_coeff = RobustGPModel(tm, 1, 'elliptical')
+sol_tm_ell_uncertain_coeff = tm_ell_uncertain_coeff.solve(verbosity=1)
 tm_ell_uncertain_coeff_robust_model = tm_ell_uncertain_coeff.robust_model
 
-tm_box_uncertain_exponents = RobustGPModel(tm, 0, 'box', two_term=True)  # this method can handle uncertain exponents,
+tm_box_uncertain_exponents = RobustGPModel(tm, 1, 'box', two_term=True)  # this method can handle uncertain exponents,
 # we do not need signomial constraints, and in most cases it is better than the method for uncertain coefficients.
-sol_tm_box_uncertain_exponents = tm_box_uncertain_exponents.solve(verbosity=0)
+sol_tm_box_uncertain_exponents = tm_box_uncertain_exponents.solve(verbosity=1)
 tm_box_uncertain_exponents_robust_model = tm_box_uncertain_exponents.robust_model
 
-tm_ell_uncertain_exponents = RobustGPModel(tm, 0, 'elliptical', two_term=True)
-sol_tm_ell_uncertain_exponents = tm_ell_uncertain_exponents.solve(verbosity=0)
+tm_ell_uncertain_exponents = RobustGPModel(tm, 1, 'elliptical', two_term=True)
+sol_tm_ell_uncertain_exponents = tm_ell_uncertain_exponents.solve(verbosity=1)
 tm_ell_uncertain_exponents_robust_model = tm_ell_uncertain_exponents.robust_model
 
 simp = models.simpleWing()
 
-simp_box_uncertain_coeff = RobustGPModel(simp, 0, 'box')
+simp_box_uncertain_coeff = RobustGPModel(simp, 1, 'box')
 # simp_box_uncertain_coeff.setup() creates the robust model and saves it in simp_box_uncertain_coeff.robust_model
-sol_simp_box_uncertain_coeff = simp_box_uncertain_coeff.solve(verbosity=0)  # calls the setup() if the robust model is not created yet,
+sol_simp_box_uncertain_coeff = simp_box_uncertain_coeff.solve(verbosity=1)  # calls the setup() if the robust model is not created yet,
 # otherwise it will directly solve the robust_model
 simp_box_uncertain_coeff_robust_model = simp_box_uncertain_coeff.robust_model
 
-simp_ell_uncertain_coeff = RobustGPModel(simp, 0, 'elliptical')
-sol_simp_ell_uncertain_coeff = simp_ell_uncertain_coeff.solve(verbosity=0)
+simp_ell_uncertain_coeff = RobustGPModel(simp, 1, 'elliptical')
+sol_simp_ell_uncertain_coeff = simp_ell_uncertain_coeff.solve(verbosity=1)
 simp_ell_uncertain_coeff_robust_model = simp_ell_uncertain_coeff.robust_model
 
-simp_box_uncertain_exponents = RobustGPModel(simp, 0, 'box', two_term=True)  # this method can handle uncertain exponents,
+simp_box_uncertain_exponents = RobustGPModel(simp, 1, 'box', two_term=True)  # this method can handle uncertain exponents,
 # we do not need signomial constraints, and in most cases it is better than the method for uncertain coefficients.
-sol_simp_box_uncertain_exponents = simp_box_uncertain_exponents.solve(verbosity=0)
+sol_simp_box_uncertain_exponents = simp_box_uncertain_exponents.solve(verbosity=1)
 simp_box_uncertain_exponents_robust_model = simp_box_uncertain_exponents.robust_model
 
-simp_ell_uncertain_exponents = RobustGPModel(simp, 0, 'elliptical', two_term=True)
-sol_simp_ell_uncertain_exponents = simp_ell_uncertain_exponents.solve(verbosity=0)
+simp_ell_uncertain_exponents = RobustGPModel(simp, 1, 'elliptical', two_term=True)
+sol_simp_ell_uncertain_exponents = simp_ell_uncertain_exponents.solve(verbosity=1)
 simp_ell_uncertain_exponents_robust_model = simp_ell_uncertain_exponents.robust_model
 
