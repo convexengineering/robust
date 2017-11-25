f1728a2bd0e4a91be4525971d1a85160f139ecc0
Ali Saab <alikassemsaab@gmail.com>
2017-11-25 00:00:03 -0500

    error!

--- GPModels.py
+++ GPModels.py
@@ -12,10 +12,10 @@ def simpleWing():
                   pr=4.225352)  # [1.7e-5 - 1.85e-5] -> [-10.982297 - -10.897739] -> 1.773414 -> 0.3865
     # pi = Variable("\\pi", np.pi, "-", "half of the circle constant", pr= 0)
     rho = Variable("\\rho", 1.23, "kg/m^3", "density of air", pr=10)  # [1.2 - 1.3] -> [0.1823 - 0.2623] -> 1.2489 -> 18
-    tau = Variable("\\tau", 0.12, "-", "airfoil thickness to chord ratio",
-                   pr=33.333333)  # [0.08 - 0.16] -> [-2.5257 - -1.8325] -> 0.1131 -> 16
-    N_ult = Variable("N_{ult}", 3.3, "-", "ultimate load factor",
-                     pr=33.333333)  # [2.2 - 4.4] -> [0.7884 - 1.4816] -> 3.1112 -> 30.5
+    tau = Variable("\\tau", 0.12, "-", "airfoil thickness to chord ratio",)
+                   # pr=33.333333)  # [0.08 - 0.16] -> [-2.5257 - -1.8325] -> 0.1131 -> 16
+    N_ult = Variable("N_{ult}", 3.3, "-", "ultimate load factor",)
+                     # pr=33.333333)  # [2.2 - 4.4] -> [0.7884 - 1.4816] -> 3.1112 -> 30.5
     V_min = Variable("V_{min}", 25, "m/s", "takeoff speed", pr=20)  # [20 - 30] -> [2.9957 - 3.4011] -> 24.4948 -> 6.33
     C_Lmax = Variable("C_{L,max}", 1.6, "-", "max CL with flaps down",
                       pr=25)  # [1.2 - 2] -> [0.1823, 0.6931] -> 1.5491 -> 58.4
@@ -28,8 +28,8 @@ def simpleWing():
                           "Wing Weight Coefficent 2", pr=66)  # [20 - 100] ->[2.9957 - 4.6051] -> 44.7213 -> 21.2
     CDA0 = Variable("(CDA0)", 0.035, "m^2", "fuselage drag area",
                     pr=42.857142)  # [0.02 - 0.05] -> [-3.9120 - -2.9957] -> 0.0316 -> 13.3
-    W_0 = Variable("W_0", 6250, "N", "aircraft weight excluding wing",
-                   pr=60)  # [2500 - 10000] -> [7.8240 - 9.2103] -> 5000 -> 8.14
+    W_0 = Variable("W_0", 6250, "N", "aircraft weight excluding wing",)
+                   # pr=60)  # [2500 - 10000] -> [7.8240 - 9.2103] -> 5000 -> 8.14
     toz = Variable("toz", 1, "-", pr=15)#k*e/tau/N_ult)  # [0.85 - 1.15] -> [-0.1625 - 0.1397] -> 0.9886 -> 1328
     ejer = Variable("ejer", 10000, "N")
     # Free Variables
