7dd28cdccdff638995c0f1959ed7c3598045c23e
Ali Saab <alikassemsaab@gmail.com>
2017-12-01 14:36:08 -0500

    substitution problem !!

--- Ned.py
+++ Ned.py
@@ -1,90 +1,14 @@
-from Robust import RobustModel
-import GPModels as Models
-from plot_feasibilities import plot_feasibilities
+import GPModels as models
+from gpkit import Model, Variable
 
 
-# for you to see the structure of the robust model, check the variables ending with _robust_model
-def test():
-    tm = Models.test_model()
+class trial(Model):
+    def setup(self, model, solution):
+        self.cost = model.cost
+        subs = {k: v for k, v in solution['freevariables'].items() if k.key.shape is None}
+        return [model], subs
 
-    # tm_box_uncertain_coeff = RobustModel(tm, 'box', two_term=False)
-    # # tm_box_uncertain_coeff.setup() creates the robust model and saves it in tm_box_uncertain_coeff.robust_model
-    # sol_tm_box_uncertain_coeff = tm_box_uncertain_coeff.robustsolve(verbosity=1)  # calls the setup() if the robust model is not created yet,
-    # # otherwise it will directly solve the robust_model
-    # tm_box_uncertain_coeff_robust_model = tm_box_uncertain_coeff.get_robust_model()
-    #
-    # tm_ell_uncertain_coeff = RobustModel(tm, 'elliptical', two_term=False)
-    # sol_tm_ell_uncertain_coeff = tm_ell_uncertain_coeff.robustsolve(verbosity=1)
-    # tm_ell_uncertain_coeff_robust_model = tm_ell_uncertain_coeff.get_robust_model()
-    #
-    # tm_box_uncertain_exponents = RobustModel(tm, 'box')  # this method can handle uncertain exponents,
-    # # we do not need signomial constraints, and in most cases it is better than the method for uncertain coefficients.
-    # sol_tm_box_uncertain_exponents = tm_box_uncertain_exponents.robustsolve(verbosity=1)
-    # tm_box_uncertain_exponents_robust_model = tm_box_uncertain_exponents.get_robust_model()
-    #
-    # tm_ell_uncertain_exponents = RobustModel(tm, 'elliptical')
-    # sol_tm_ell_uncertain_exponents = tm_ell_uncertain_exponents.robustsolve(verbosity=1)
-    # tm_ell_uncertain_exponents_robust_model = tm_ell_uncertain_exponents.get_robust_model()
-
-    # simp = Models.simpleWing()
-    # simp.solve()
-    # simp_box_uncertain_coeff = RobustModel(simp, 'box', two_term=False)
-    # # simp_box_uncertain_coeff.setup() creates the robust model and saves it in simp_box_uncertain_coeff.robust_model
-    # sol_simp_box_uncertain_coeff = simp_box_uncertain_coeff.robustsolve(verbosity=1)  # calls the setup() if the robust model is not created yet,
-    # # otherwise it will directly solve the robust_model
-    # simp_box_uncertain_coeff_robust_model = simp_box_uncertain_coeff.get_robust_model()
-    # plot_feasibilities(simp["W_{W_{coeff1}}"], simp["W_{W_{coeff2}}"], simp,
-    #                    simp_box_uncertain_coeff_robust_model, "box")
-
-
-    simp = Models.simpleWing()
-    simp.solve()
-    # plot_feasibilities(simp.W1, simp.W2, simp)
-    simp.W1.key.descr["pr"] = 67
-    simp.W2.key.descr["pr"] = 67
-    simp_ell_uncertain_coeff = RobustModel(simp, 'elliptical', two_term=False)
-    sol_simp_ell_uncertain_coeff = simp_ell_uncertain_coeff.robustsolve(verbosity=1)
-    simp_ell_uncertain_coeff_robust_model = simp_ell_uncertain_coeff.get_robust_model()
-    plot_feasibilities(simp.W1, simp.W2, simp,
-                       simp_ell_uncertain_coeff_robust_model, "elliptical")
-
-    # simp = Models.simpleWing()
-    # simp.solve()
-    # simp_box_uncertain_exponents = RobustModel(simp, 'box')  # this method can handle uncertain exponents,
-    # # we do not need signomial constraints, and in most cases it is better than the method for uncertain coefficients.
-    # sol_simp_box_uncertain_exponents = simp_box_uncertain_exponents.robustsolve(verbosity=1)
-    # simp_box_uncertain_exponents_robust_model = simp_box_uncertain_exponents.get_robust_model()
-    # plot_feasibilities(simp["W_{W_{coeff1}}"], simp["W_{W_{coeff2}}"], simp,
-    #                    simp_box_uncertain_exponents_robust_model, "box")
-    #
-    # simp = Models.simpleWing()
-    # simp.solve()
-    # simp_ell_uncertain_exponents = RobustModel(simp, 'elliptical')
-    # sol_simp_ell_uncertain_exponents = simp_ell_uncertain_exponents.robustsolve(verbosity=1)
-    # simp_ell_uncertain_exponents_robust_model = simp_ell_uncertain_exponents.get_robust_model()
-    # plot_feasibilities(simp["W_{W_{coeff1}}"], simp["W_{W_{coeff2}}"], simp,
-    #                    simp_ell_uncertain_exponents_robust_model, "elliptical")
-    #
-    # simpSP = Models.simpleWing()
-    #
-    # simp_box_uncertain_coeff = RobustModel(simpSP, 'box', two_term=False)
-    # # simp_box_uncertain_coeff.setup() creates the robust model and saves it in simp_box_uncertain_coeff.robust_model
-    # sol_simp_box_uncertain_coeff = simp_box_uncertain_coeff.robustsolve(verbosity=1)  # calls the setup() if the robust model is not created yet,
-    # # otherwise it will directly solve the robust_model
-    # simp_box_uncertain_coeff_robust_model = simp_box_uncertain_coeff.get_robust_model()
-    #
-    # simp_ell_uncertain_coeff = RobustModel(simpSP, 'elliptical', two_term=False)
-    # sol_simp_ell_uncertain_coeff = simp_ell_uncertain_coeff.robustsolve(verbosity=1)
-    # simp_ell_uncertain_coeff_robust_model = simp_ell_uncertain_coeff.get_robust_model()
-    #
-    # simp_box_uncertain_exponents = RobustModel(simpSP, 'box')  # this method can handle uncertain exponents,
-    # # we do not need signomial constraints, and in most cases it is better than the method for uncertain coefficients.
-    # sol_simp_box_uncertain_exponents = simp_box_uncertain_exponents.robustsolve(verbosity=1)
-    # simp_box_uncertain_exponents_robust_model = simp_box_uncertain_exponents.get_robust_model()
-    #
-    # simp_ell_uncertain_exponents = RobustModel(simpSP, 'elliptical')
-    # sol_simp_ell_uncertain_exponents = simp_ell_uncertain_exponents.robustsolve(verbosity=1)
-    # simp_ell_uncertain_exponents_robust_model = simp_ell_uncertain_exponents.get_robust_model()
-
-if __name__ == "__main__":
-    test()
+solar = models.mike_solar_model()
+solution = solar.solve()
+new_solar = trial(solar, solution)
+new_solution = new_solar.solve()
