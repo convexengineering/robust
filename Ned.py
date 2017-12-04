97d6d30b974358466841ae0a76931686addba4c0
Ali Saab <alikassemsaab@gmail.com>
2017-12-04 14:18:08 -0500

    added dummy var

--- Ned.py
+++ Ned.py
@@ -1,12 +1,14 @@
 import GPModels as models
-from gpkit import Model, Variable
+from gpkit import Model, Variable, ConstraintSet
 
 
 class trial(Model):
     def setup(self, model, solution):
         self.cost = model.cost
-        subs = {k: v for k, v in solution['freevariables'].items() if k.key.shape is None}
-        return [model], subs
+        subs = {k: v for k, v in solution['freevariables'].items() if not hasattr(v, "__call__")}
+        slack = Variable
+        ConstraintSet([slack <= 10, slack <= 10])
+        return [model, ConstraintSet([slack <= 10, slack <= 10])], subs
 
 solar = models.mike_solar_model()
 solution = solar.solve()
