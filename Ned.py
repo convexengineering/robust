d47e9c6d1801fd82a33289753282afb676386554
Ali Saab <alikassemsaab@gmail.com>
2017-12-05 15:57:37 -0500

    empty Ned file

--- Ned.py
+++ Ned.py
@@ -1,16 +1,1 @@
-import GPModels as models
-from gpkit import Model, Variable, ConstraintSet
-
-
-class trial(Model):
-    def setup(self, model, solution):
-        self.cost = model.cost
-        subs = {k: v for k, v in solution['freevariables'].items() if not hasattr(v, "__call__")}
-        slack = Variable
-        ConstraintSet([slack <= 10, slack <= 10])
-        return [model, ConstraintSet([slack <= 10, slack >= 1])], subs
-
-solar = models.mike_solar_model()
-solution = solar.solve()
-new_solar = trial(solar, solution)
-new_solution = new_solar.solve()
+#Nothing
