d89d6f8e969d33d85600eaccf674924a2bb9c938
Ali Saab <alikassemsaab@gmail.com>
2018-06-03 14:40:52 -0400

    deleted

--- TwoDimensionalTestProblem.py
+++
@@ -1,6 +0,0 @@
-import GPModels as GPM
-import RobustGP as RGP
-
-model = GPM.testModel()
-robustModel = RGP.robustModelEllipticalUncertainty(model, linearizeTwoTerm=False, enableSP=True, numberOfRegressionPoints=2)[0]
-GPM.solveModel(robustModel);
