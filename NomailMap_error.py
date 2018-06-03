e171e213de16a6ad6b9c9b6ddc41c3a0650e247c
Ali Saab <alikassemsaab@gmail.com>
2018-06-03 06:42:34 -0400

    deleted

--- NomailMap_error.py
+++
@@ -1,9 +0,0 @@
-from RobustGP import RobustGPModel
-from gpkit import Model
-import numpy as np
-import GPModels
-
-simp = GPModels.simpleWingSP()
-solution = simp.localsolve().get('variables')
-print(solution)
-local_gp_approximation = Model(simp.cost, simp.as_gpconstr(solution))
--- temp.py
+++
@@ -1,5 +0,0 @@
-from whatever import whatever
-
-rgpp = RobustGPParameters(*args, **kwargs)
-
-rggp2 = RobustGPParameters.from_robustgpmodel(rgpm)
