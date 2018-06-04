f09ae978869a981a1ff2a02fee5bdc69e58e6161
Ali Saab <alikassemsaab@gmail.com>
2018-06-04 00:37:36 -0400

    update

--- data/constructLinearizationData.py
+++ data/constructLinearizationData.py
@@ -3,14 +3,14 @@ import numpy as np
 
 
 def construct_linearization_data(max_num_of_linear_sections):
-    file = open("data/linearization_data.txt", "w")
+    the_file = open("/home/saab/Dropbox (MIT)/MIT/Masters/Code/robust/data/linearization_data.txt", "w")
     for i in range(2, max_num_of_linear_sections):
         print i
         a = LinearizeTwoTermPosynomials.compute_two_term_posynomial_linearization_coeff(i, 2*np.finfo(float).eps)
         for j in xrange(4):
             for item in a[j]:
-                file.write("%s, " % item)
-            file.write(": ")
-        file.write("%.16s" % a[4])
-        file.write("\n")
-    file.close()
+                the_file.write("%s, " % item)
+            the_file.write(": ")
+        the_file.write("%.16s" % a[4])
+        the_file.write("\n")
+    the_file.close()
