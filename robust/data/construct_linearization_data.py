23f7e9c6b20096741b5e238163faea16d5956629
Ali Saab <alikassemsaab@gmail.com>
2018-07-01 15:45:24 -0400

    user specified file path

--- robust/data/construct_linearization_data.py
+++ robust/data/construct_linearization_data.py
@@ -4,8 +4,8 @@ from robust.linearize_twoterm_posynomials import LinearizeTwoTermPosynomials
 import os
 
 
-def construct_linearization_data(max_num_of_linear_sections):
-    the_file = open(os.path.dirname(__file__) + "/linearization_data.txt", "w")
+def construct_linearization_data(max_num_of_linear_sections, the_file_path):
+    the_file = open(the_file_path, "w")
     for i in range(2, max_num_of_linear_sections):
         print i
         a = LinearizeTwoTermPosynomials.compute_two_term_posynomial_linearization_coeff(i, 2*np.finfo(float).eps)
@@ -18,4 +18,4 @@ def construct_linearization_data(max_num_of_linear_sections):
     the_file.close()
 
 if __name__ == '__main__':
-    construct_linearization_data(100)
+    construct_linearization_data(100, os.path.dirname(__file__) + "/linearization_data.txt")
