64e258b79693670119201d6f9913d5bb64eb3205
Ali Saab <alikassemsaab@gmail.com>
2018-08-01 09:15:43 -0400

    update

--- build/lib.linux-x86_64-2.7/robust/prereq.py
+++ build/lib.linux-x86_64-2.7/robust/prereq.py
@@ -4,10 +4,14 @@ from simulations.simulate import generate_model_properties
 
 simp = simple_wing_sp()
 _, _, _, uncertain_vars = generate_model_properties(simp, 1, 1)
+print simp
 sol = simp.localsolve(verbosity=0)
+print simp
 new_model = RobustGPTools.DesignedModel(simp, sol, uncertain_vars[0])
-try:
-    new_model.localsolve()
-except ValueError:
-    pass
+print new_model
+# try:
+new_model.localsolve()
+# except ValueError:
+#     pass
+print simp
 _ = simp.localsolve()
