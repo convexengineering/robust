Sorted bug in average cost calculation.

robust/robust_gp_tools.py
@@ -110,10 +110,10 @@ class RobustGPTools:
         costs = [0 if results[i] is None else mag(results[i]) for i in range(number_of_iterations)]
         print costs
         if np.sum(costs) > 0:
-            cost_average = np.sum(costs) / (np.sum(costs > 0.) + 0.0)
+            cost_average = np.sum(costs) / (len(np.nonzero(costs)[0]) + 0.0)
         else:
             cost_average = None
-        prob = 1. - (np.sum(costs > 0.)/(number_of_iterations + 0.0))
+        prob = 1. - (len(np.nonzero(costs)[0])/(number_of_iterations + 0.0))
         return prob, cost_average
 
     class DesignedModel(Model):