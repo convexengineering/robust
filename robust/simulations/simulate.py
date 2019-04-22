e44269ab5817f144e929df9a47faeeeb6fa36a04
1ozturkbe <1ozturkbe@gmail.com>
2019-04-22 13:40:50 -0400

    clarification in nomenclature.

--- robust/simulations/simulate.py
+++ robust/simulations/simulate.py
@@ -191,16 +191,16 @@ def variable_goal_results(model, methods, deltas, number_of_iterations,
         mGoal.substitutions.update({'1+\\delta': 1 + delta})
         for method in methods:
             for uncertainty_set in uncertainty_sets:
-                robust_model = RobustModel(mGoal, uncertainty_set, gamma=Gamma, twoTerm=method['twoTerm'],
+                robust_goal_model = RobustModel(mGoal, uncertainty_set, gamma=Gamma, twoTerm=method['twoTerm'],
                                    boyd=method['boyd'], simpleModel=method['simpleModel'])
 
-                robust_model_solution = robust_model.robustsolve(verbosity=verbosity,
+                robust_model_solution = robust_goal_model.robustsolve(verbosity=verbosity,
                                                              minNumOfLinearSections=min_num_of_linear_sections,
                                                              maxNumOfLinearSections=max_num_of_linear_sections,
                                                              linearizationTolerance=linearization_tolerance)
 
                 robust_model_solve_time = get_avg_robust_solve_time(number_of_time_average_solves,
-                                                        robust_model, verbosity, min_num_of_linear_sections,
+                                                        robust_goal_model, verbosity, min_num_of_linear_sections,
                                                         max_num_of_linear_sections,
                                                         linearization_tolerance, parallel)
 
@@ -210,10 +210,10 @@ def variable_goal_results(model, methods, deltas, number_of_iterations,
                                                                   verbosity=0, parallel=parallel)
                 try:
                     nconstraints = \
-                        len([cnstrnt for cnstrnt in robust_model.get_robust_model().flat(constraintsets=False)])
+                        len([cnstrnt for cnstrnt in robust_goal_model.get_robust_model().flat(constraintsets=False)])
                 except AttributeError:
                     nconstraints = \
-                        len([cnstrnt for cnstrnt in robust_model.get_robust_model()[-1].flat(constraintsets=False)])
+                        len([cnstrnt for cnstrnt in robust_goal_model.get_robust_model()[-1].flat(constraintsets=False)])
                 ind = (delta, method['name'], uncertainty_set)
                 solutions[ind] = robust_model_solution
                 solve_times[ind] = robust_model_solve_time
