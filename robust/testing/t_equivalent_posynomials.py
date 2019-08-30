48f230e0dbdf9700af7e47a8c4e518ccdb3e4459
1ozturkbe <1ozturkbe@gmail.com>
2019-08-30 10:03:34 -0400

    t_equivalent_posynomials.

--- robust/testing/t_equivalent_posynomials.py
+++ robust/testing/t_equivalent_posynomials.py
@@ -3,136 +3,135 @@ from builtins import range
 from gpkit import Variable, Monomial
 import numpy as np
 
+import unittest
+from gpkit.tests.helpers import run_tests
+
 from robust.equivalent_posynomials import EquivalentPosynomials
 
-def test_merge_intersected_lists():
-    for _ in range(100):
-        number_of_lists = int(10*np.random.random()) + 1
-        list_of_lists = []
-        for l in range(number_of_lists):
-            number_of_elements = int(5*np.random.random()) + 1
-            list_of_lists.append([int(20*np.random.random()) for _ in range(number_of_elements)])
-        partition = EquivalentPosynomials.merge_intersected_lists(list_of_lists)
-        for i in range(len(partition) - 1):
-            assert (all(not (set(partition[i]) & set(partition[j])) for j in range(i+1, len(partition))))
-        for from_list in list_of_lists:
-            assert(any(set(from_list) <= set(from_partition) for from_partition in partition))
+class TestEquivalentPosynomials(unittest.TestCase):
+    def test_merge_intersected_lists(self):
+        for _ in range(100):
+            number_of_lists = int(10*np.random.random()) + 1
+            list_of_lists = []
+            for l in range(number_of_lists):
+                number_of_elements = int(5*np.random.random()) + 1
+                list_of_lists.append([int(20*np.random.random()) for _ in range(number_of_elements)])
+            partition = EquivalentPosynomials.merge_intersected_lists(list_of_lists)
+            for i in range(len(partition) - 1):
+                self.assertTrue(all(not (set(partition[i]) & set(partition[j])) for j in range(i+1, len(partition))))
+            for from_list in list_of_lists:
+                self.assertTrue(any(set(from_list) <= set(from_partition) for from_partition in partition))
 
-def test_same_sign():
-    for _ in range(20):
-        number_of_elements = int(50*np.random.random()) + 1
-        a = [int(20*np.random.random()) for _ in range(number_of_elements)]
-        assert(EquivalentPosynomials.same_sign(a))
-        a = [int(20*np.random.random()) - 19 for _ in range(number_of_elements)]
-        assert(EquivalentPosynomials.same_sign(a))
-        a = [int(20*np.random.random()) - 19 for _ in range(number_of_elements)]
-        neg_flag = False
-        for element in a:
-            if element < 0:
-                neg_flag = True
-                break
-        pos_flag = False
-        for element in a:
-            if element > 0:
-                pos_flag = True
-                break
+    def test_same_sign(self):
+        for _ in range(20):
+            number_of_elements = int(50*np.random.random()) + 1
+            a = [int(20*np.random.random()) for _ in range(number_of_elements)]
+            self.assertTrue(EquivalentPosynomials.same_sign(a))
+            a = [int(20*np.random.random()) - 19 for _ in range(number_of_elements)]
+            self.assertTrue(EquivalentPosynomials.same_sign(a))
+            a = [int(20*np.random.random()) - 19 for _ in range(number_of_elements)]
+            neg_flag = False
+            for element in a:
+                if element < 0:
+                    neg_flag = True
+                    break
+            pos_flag = False
+            for element in a:
+                if element > 0:
+                    pos_flag = True
+                    break
 
-        if neg_flag and pos_flag:
-            assert(not EquivalentPosynomials.same_sign(a))
-        else:
-            assert(EquivalentPosynomials.same_sign(a))
+            if neg_flag and pos_flag:
+                self.assertFalse(EquivalentPosynomials.same_sign(a))
+            else:
+                self.assertTrue(EquivalentPosynomials.same_sign(a))
 
-def test_correlated_monomials():
-    for _ in range(20):
-        number_of_monomials = int(50*np.random.random())+1
-        number_of_gp_variables = int(np.random.rand()*20) + 1
-        number_of_uncertain_variables = int(np.random.rand()*5) + 1
-        vector_to_choose_from = [0, 0, 0, 0, 0, 0, 0, 0, 1, -1]
+    def test_correlated_monomials(self):
+        for _ in range(20):
+            number_of_monomials = int(50*np.random.random())+1
+            number_of_gp_variables = int(np.random.rand()*20) + 1
+            number_of_uncertain_variables = int(np.random.rand()*5) + 1
+            vector_to_choose_from = [0, 0, 0, 0, 0, 0, 0, 0, 1, -1]
 
-        m = number_of_monomials*[1]
-        p_uncertain_vars = []
-        correlations = []
-        dependent_theoretical_partition = []
+            m = number_of_monomials*[1]
+            p_uncertain_vars = []
+            correlations = []
+            dependent_theoretical_partition = []
 
-        # Generating random monomials
-        for j in range(number_of_monomials):
-            for i in range(number_of_gp_variables):
-                x = Variable('x_%s' % i)
-                m[j] *= x**(np.random.rand()*10 - 5)
-
-        # Generating random uncertain variables to merge with monomials
-        for i in range(number_of_uncertain_variables):
-            u = Variable('u_%s' % i, np.random.random(), pr=100*np.random.random())
-            p_uncertain_vars.append(u.key)
-            neg_pos_neutral_powers = [vector_to_choose_from[int(10*np.random.random())] for _ in range(number_of_monomials)]
-            same_sign = EquivalentPosynomials.same_sign(neg_pos_neutral_powers)
-
-            if not same_sign:
-                related_monomials = [i for i in range(number_of_monomials) if neg_pos_neutral_powers[i] != 0]
-                correlations.append(related_monomials)
-
+            # Generating random monomials
             for j in range(number_of_monomials):
-                m[j] *= u**(np.random.rand()*5*neg_pos_neutral_powers[j])
-                if neg_pos_neutral_powers[j] != 0:
-                    dependent_theoretical_partition.append(j)
+                for i in range(number_of_gp_variables):
+                    x = Variable('x_%s' % i)
+                    m[j] *= x**(np.random.rand()*10 - 5)
 
-        # Merging intersections of correlations gives the theoretical partition of monomials.
-        theoretical_partition = EquivalentPosynomials.merge_intersected_lists(correlations)
+            # Generating random uncertain variables to merge with monomials
+            for i in range(number_of_uncertain_variables):
+                u = Variable('u_%s' % i, np.random.random(), pr=100*np.random.random())
+                p_uncertain_vars.append(u.key)
+                neg_pos_neutral_powers = [vector_to_choose_from[int(10*np.random.random())] for _ in range(number_of_monomials)]
+                same_sign = EquivalentPosynomials.same_sign(neg_pos_neutral_powers)
 
-        p = sum(m)
-        equivalent_posynomial = EquivalentPosynomials(p, 0, False, False)
-        equivalent_posynomial.main_p = p
-        actual_partition = equivalent_posynomial.correlated_monomials()
+                if not same_sign:
+                    related_monomials = [i for i in range(number_of_monomials) if neg_pos_neutral_powers[i] != 0]
+                    correlations.append(related_monomials)
 
-        theoretical_posynomials = []
-        actual_posynomials = []
+                for j in range(number_of_monomials):
+                    m[j] *= u**(np.random.rand()*5*neg_pos_neutral_powers[j])
+                    if neg_pos_neutral_powers[j] != 0:
+                        dependent_theoretical_partition.append(j)
 
-        monomials = p.chop()
-        for theo_list, act_list in zip(theoretical_partition, actual_partition):
-            theoretical_posynomials.append(sum([m[i] for i in theo_list]))
-            actual_posynomials.append(sum([monomials[j] for j in act_list]))
+            # Merging intersections of correlations gives the theoretical partition of monomials.
+            theoretical_partition = EquivalentPosynomials.merge_intersected_lists(correlations)
 
-        assert (set(actual_posynomials) == set(theoretical_posynomials))
+            p = sum(m)
+            equivalent_posynomial = EquivalentPosynomials(p, 0, False, False)
+            equivalent_posynomial.main_p = p
+            actual_partition = equivalent_posynomial.correlated_monomials()
 
-        # dependent uncertainties
-        temp = []
-        for part in theoretical_partition:
-            temp += part
+            theoretical_posynomials = []
+            actual_posynomials = []
 
-        theoretical_partition = list(set(dependent_theoretical_partition))
-        equivalent_posynomial = EquivalentPosynomials(p, 0, False, True)
-        equivalent_posynomial.main_p = p
-        actual_partition = equivalent_posynomial.correlated_monomials()
+            monomials = p.chop()
+            for theo_list, act_list in zip(theoretical_partition, actual_partition):
+                theoretical_posynomials.append(sum([m[i] for i in theo_list]))
+                actual_posynomials.append(sum([monomials[j] for j in act_list]))
 
-        theoretical_posynomials = []
-        actual_posynomials = []
+            self.assertSetEqual(set(actual_posynomials), set(theoretical_posynomials))
 
-        theoretical_posynomials.append(sum([m[i] for i in theoretical_partition]))
-        actual_posynomials.append(sum([monomials[j] for j in actual_partition[0]]))
-        assert (list(actual_posynomials) == list(theoretical_posynomials))
+            # dependent uncertainties
+            temp = []
+            for part in theoretical_partition:
+                temp += part
 
-def test_check_if_in_list_of_lists():
-    for _ in range(20):
-        number_of_lists = int(10*np.random.random()) + 1
-        list_of_lists = []
-        number_of_elements = []
-        for l in range(number_of_lists):
-            number_of_elements.append(int(5*np.random.random()) + 1)
-            list_of_lists.append([int(200*np.random.random()) for _ in range(number_of_elements[l])])
+            theoretical_partition = list(set(dependent_theoretical_partition))
+            equivalent_posynomial = EquivalentPosynomials(p, 0, False, True)
+            equivalent_posynomial.main_p = p
+            actual_partition = equivalent_posynomial.correlated_monomials()
 
-        element_list = int(number_of_lists*np.random.random())
-        element = list_of_lists[element_list][int(number_of_elements[element_list]*np.random.random())]
-        non_element = int(200*np.random.random()) + 200
+            theoretical_posynomials = []
+            actual_posynomials = []
 
-        assert (EquivalentPosynomials.check_if_in_list_of_lists(element, list_of_lists))
-        assert (not EquivalentPosynomials.check_if_in_list_of_lists(non_element, list_of_lists))
+            theoretical_posynomials.append(sum([m[i] for i in theoretical_partition]))
+            actual_posynomials.append(sum([monomials[j] for j in actual_partition[0]]))
+            self.assertSetEqual(set(actual_posynomials), set(theoretical_posynomials))
 
-def test():
-    test_same_sign()
-    test_merge_intersected_lists()
-    test_correlated_monomials()
-    test_check_if_in_list_of_lists()
+    def test_check_if_in_list_of_lists(self):
+        for _ in range(20):
+            number_of_lists = int(10*np.random.random()) + 1
+            list_of_lists = []
+            number_of_elements = []
+            for l in range(number_of_lists):
+                number_of_elements.append(int(5*np.random.random()) + 1)
+                list_of_lists.append([int(200*np.random.random()) for _ in range(number_of_elements[l])])
 
+            element_list = int(number_of_lists*np.random.random())
+            element = list_of_lists[element_list][int(number_of_elements[element_list]*np.random.random())]
+            non_element = int(200*np.random.random()) + 200
+
+            self.assertTrue(EquivalentPosynomials.check_if_in_list_of_lists(element, list_of_lists))
+            self.assertFalse(EquivalentPosynomials.check_if_in_list_of_lists(non_element, list_of_lists))
+
+TESTS = [TestEquivalentPosynomials]
 
 if __name__ == "__main__":
-    test()
+    run_tests(TESTS)
