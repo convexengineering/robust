092723c55b47db3ff9a5a25b34ca3c39a1d853b9
1ozturkbe <1ozturkbe@gmail.com>
2019-09-11 15:22:38 -0400

    Testing simulations with simple_wing.

--- robust/testing/t_simulation.py
+++ robust/testing/t_simulation.py
@@ -1,16 +1,16 @@
 import numpy as np
-
 import unittest
 from gpkit.tests.helpers import run_tests
 
 from robust.simulations import simulate
-from robust.testing.models import gp_test_model
+from robust.simple_wing.models import simple_wing
+from robust.testing.models import primitive_model
 
 class TestSimulation(unittest.TestCase):
     def test_simulate(self):
-        model = gp_test_model()
+        model = simple_wing()
         number_of_time_average_solves = 3
-        number_of_iterations = 10
+        number_of_iterations = 20
         uncertainty_sets = ['box']
         methods = [{'name': 'Best Pairs', 'twoTerm': True, 'boyd': False, 'simpleModel': False}]
         nGammas = 3
@@ -19,21 +19,31 @@ class TestSimulation(unittest.TestCase):
         max_num_of_linear_sections = 99
         linearization_tolerance = 1e-4
         verbosity = 0
-        parallel = False
 
         nominal_solution, nominal_solve_time, nominal_number_of_constraints, directly_uncertain_vars_subs = \
             simulate.generate_model_properties(model, number_of_time_average_solves, number_of_iterations)
 
-        solutions, solve_times, simulation_results, number_of_constraints = simulate.variable_gamma_results(
+        _, _, simulation_results, _ = simulate.variable_gamma_results(
             model, methods, gammas, number_of_iterations,
             min_num_of_linear_sections,
             max_num_of_linear_sections, verbosity, linearization_tolerance,
             number_of_time_average_solves,
-            uncertainty_sets, nominal_solution, directly_uncertain_vars_subs, parallel=parallel)
+            uncertainty_sets, nominal_solution, directly_uncertain_vars_subs, parallel=False)
 
         # Checking probability of failure is 0 for gamma=1
-        robustifiedResult = simulation_results[sorted(simulation_results.keys())[-1]]
-        self.assertEqual(robustifiedResult[0], 0.)
+        keys = sorted(simulation_results.keys())
+        self.assertEqual(simulation_results[keys[-1]][0], 0.)
+
+        # Then test in parallel, and compare time results
+        # _, _, parallel_simulation_results, _ = simulate.variable_gamma_results(
+        #     model, methods, gammas, number_of_iterations,
+        #     min_num_of_linear_sections,
+        #     max_num_of_linear_sections, verbosity, linearization_tolerance,
+        #     number_of_time_average_solves,
+        #     uncertainty_sets, nominal_solution, directly_uncertain_vars_subs, parallel=True)
+
+        # Checking mean of simulation results is equal for gamma=0
+        # self.assertAlmostEqual(simulation_results[keys[0]][1], parallel_simulation_results[keys[0]][1])
 
 TESTS = [TestSimulation]
 
