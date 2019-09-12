e14c5f30bc13daba7ccc0a9f2e0623489e0db512
1ozturkbe <1ozturkbe@gmail.com>
2019-09-12 11:35:31 -0400

    Hacking t_legacy for now.

--- robust/testing/t_legacy.py
+++ robust/testing/t_legacy.py
@@ -3,7 +3,9 @@ import numpy as np
 import os
 
 import unittest
+from gpkit import Model
 from gpkit.tests.helpers import run_tests
+from gpkit.constraints.bounded import Bounded
 
 from robust.testing.models import simple_wing
 from robust.simulations import simulate
@@ -11,13 +13,13 @@ from robust.simulations import read_simulation_data
 
 class TestLegacy(unittest.TestCase):
     def test_simple_wing(self):
-        model = simple_wing()
+        model = Model(simple_wing().cost, Bounded(simple_wing()), simple_wing().substitutions)
         number_of_time_average_solves = 3
         number_of_iterations = 10
         nominal_solution, nominal_solve_time, nominal_number_of_constraints, directly_uncertain_vars_subs = \
             simulate.generate_model_properties(model, number_of_time_average_solves, number_of_iterations)
-        model_name = 'Simple Wing'
-        gammas = np.linspace(0, 0.75, 3)
+        model_name = 'Simple_Wing'
+        gammas = np.linspace(0.0, 1.0, 3)
         min_num_of_linear_sections = 3
         max_num_of_linear_sections = 99
         linearization_tolerance = 1e-4
@@ -29,7 +31,8 @@ class TestLegacy(unittest.TestCase):
                    {'name': 'Two Term', 'twoTerm': False, 'boyd': True, 'simpleModel': False}]
         uncertainty_sets = ['box', 'elliptical']
 
-        variable_gamma_file_name = os.path.dirname(__file__) + '/simulation_data_variable_gamma.txt'
+        variable_gamma_file_name = os.path.dirname(__file__) + '/' + \
+                                   model_name + '_simulation_data_variable_gamma.txt'
         simulate.print_variable_gamma_results(model, model_name, gammas, number_of_iterations,
                                                  min_num_of_linear_sections,
                                                  max_num_of_linear_sections, verbosity, linearization_tolerance,
@@ -45,7 +48,8 @@ class TestLegacy(unittest.TestCase):
                    {'name': 'Two Term', 'twoTerm': False, 'boyd': True, 'simpleModel': False}]
         uncertainty_sets = ['box', 'elliptical']
 
-        variable_pwl_file_name = os.path.dirname(__file__) + '/simulation_data_variable_pwl.txt'
+        variable_pwl_file_name = os.path.dirname(__file__) + '/' + \
+                                 model_name + '_simulation_data_variable_pwl.txt'
         simulate.print_variable_pwlsections_results(model, model_name, gamma, number_of_iterations,
                                                                    numbers_of_linear_sections, linearization_tolerance,
                                                                    verbosity, variable_pwl_file_name,
@@ -53,9 +57,8 @@ class TestLegacy(unittest.TestCase):
                                                                    nominal_solution, nominal_solve_time,
                                                                    nominal_number_of_constraints, directly_uncertain_vars_subs)
 
-        file_path_gamma = os.path.dirname(__file__) + '/simulation_data_variable_gamma.txt'
-        file_path_pwl = os.path.dirname(__file__) + '/simulation_data_variable_pwl.txt'
-        read_simulation_data.generate_all_plots(file_path_gamma, file_path_pwl)
+        read_simulation_data.generate_all_plots(variable_gamma_file_name,
+                                                variable_pwl_file_name)
 
 TESTS = [TestLegacy]
 
