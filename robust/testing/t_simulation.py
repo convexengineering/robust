f2ae93cde58e5ce9b50efcbcb3530268d9a1d91b
1ozturkbe <1ozturkbe@gmail.com>
2019-09-18 14:25:42 -0400

    Adding diff test to simulations.

--- robust/testing/t_simulation.py
+++ robust/testing/t_simulation.py
@@ -1,15 +1,21 @@
 import numpy as np
+import os
 import unittest
 from gpkit.tests.helpers import run_tests
-from gpkit import Model
-from gpkit.constraints.bounded import Bounded
+from gpkit.small_scripts import mag
 
+from robust.margin import MarginModel
+from robust.robust import RobustModel
 from robust.simulations import simulate
 from robust.testing.models import simple_ac
+
+def round_sig(x, sig=2):
+    return np.round(x, sig-int(np.floor(np.log10(np.abs(x))))-1)
 
 class TestSimulation(unittest.TestCase):
     def test_simulate(self):
         model = simple_ac()
+        model.cost = model['c']
         number_of_time_average_solves = 3
         number_of_iterations = 20
         uncertainty_sets = ['elliptical']
@@ -45,6 +51,45 @@ class TestSimulation(unittest.TestCase):
 
         # Checking mean of simulation results is equal for gamma=0
         # self.assertAlmostEqual(simulation_results[keys[0]][1], parallel_simulation_results[keys[0]][1])
+
+    def test_table_diff(self):
+        m = simple_ac()
+        m.cost = m['c']
+        sol = m.localsolve(verbosity=0)
+
+        # Model with margins
+        mm = MarginModel(m) 
+        msol = mm.localsolve(verbosity=0)
+        # Model with box uncertainty
+        bm = RobustModel(m, 'box', twoTerm = True, boyd = False, simpleModel = False)
+        bsol = bm.robustsolve(verbosity=0)
+        # Model with elliptical uncertainty
+        em = RobustModel(m, 'elliptical', twoTerm = True, boyd = False, simpleModel = False)
+        esol = em.robustsolve(verbosity=0)
+
+        soltab = [sol, msol, bsol, esol]
+
+        origfilename = os.path.dirname(__file__) + '/' + 'test_table.txt'
+        filename = os.path.dirname(__file__) + '/' + 'test_table_diff.txt'
+        f = open(filename, 'w+')
+
+        for i in ['L/D', 'A', 'Re', 'S', 'V', 't_s', 'W_w', 'W_{w_{strc}}', 'W_{w_{surf}}',
+              'W_{fuse}','V_{f_{avail}}', 'V_{f_{fuse}}', 'V_{f_{wing}}']:
+            f.write(i)
+            if i in ['L/D', 'Re', 'V']:
+                a = [mag(np.mean(s(i))) for s in soltab]
+            elif i in ['t_s']:
+                a = [mag(np.sum(s(i))) for s in soltab]
+            else:
+                a = [mag(s(i))  for s in soltab]
+            f.write(''.join([" & " + str(round_sig(j,3)) for j in a]))
+            f.write('\n')
+        f.write('cost')
+        f.write(' '.join([" & " + str(i['cost']) for i in soltab]))
+        f.write('\n')
+        f.close()
+
+        self.assertEqual(open(origfilename, 'r').readlines(), open(filename, 'r').readlines())
 
 TESTS = [TestSimulation]
 
