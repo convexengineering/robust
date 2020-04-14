b8b8e0fa6f8f86c09d210e79afbcd977da3dead0
1ozturkbe <1ozturkbe@gmail.com>
2020-04-14 16:29:38 -0400

    First steps to fixing t_simulation.

--- robust/testing/t_simulation.py
+++ robust/testing/t_simulation.py
@@ -8,6 +8,7 @@ from robust.margin import MarginModel
 from robust.robust import RobustModel
 from robust.simulations import simulate
 from robust.testing.models import simple_ac
+import pickle
 
 class TestSimulation(unittest.TestCase):
     solver = None
@@ -74,29 +75,33 @@ class TestSimulation(unittest.TestCase):
         esol = em.robustsolve(verbosity=0)
 
         soltab = [sol, msol, bsol, esol]
+        diff_idx = ['sol', 'msol', 'bsol', 'esol']
 
-        origfilename = os.path.dirname(__file__) + '/' + 'diffs/test_table.txt'
-        filename = os.path.dirname(__file__) + '/' + 'diffs/test_table_diff.txt'
-        f = open(filename, 'w+')
+        for i in range(len(soltab)):
+            # soltab[i].save(filename=os.path.dirname(__file__) + '/diffs/' + diff_idx[i] + '.pkl')
+            # soltab[i].save('diffs/' + diff_idx[i] + '.pkl')
+            origsol = pickle
 
-        for i in ['L/D', 'A', 'Re', 'S', 'V', 't_s', 'W_w', 'W_{w_{strc}}', 'W_{w_{surf}}',
-              'W_{fuse}','V_{f_{avail}}', 'V_{f_{fuse}}', 'V_{f_{wing}}']:
-            f.write(i)
-            if i in ['L/D', 'Re', 'V']:
-                a = [mag(np.mean(s(i))) for s in soltab]
-            elif i in ['t_s']:
-                a = [mag(np.sum(s(i))) for s in soltab]
-            else:
-                a = [mag(s(i))  for s in soltab]
-            for j in range(len(a)):
-                if a[j] <= 1e-5:
-                    a[j] = 0.
-            f.write(''.join([" & " + str(np.format_float_scientific(j, precision=2)) for j in a]))
-            f.write('\n')
-        f.write('cost ')
-        f.write(' '.join(["& " + str(np.format_float_scientific(i['cost'], precision=2)) for i in soltab]))
-        f.write('\n')
-        f.close()
+        # f = open(filename, 'w+')
+        #
+        # for i in ['L/D', 'A', 'Re', 'S', 'V', 't_s', 'W_w', 'W_{w_{strc}}', 'W_{w_{surf}}',
+        #       'W_{fuse}','V_{f_{avail}}', 'V_{f_{fuse}}', 'V_{f_{wing}}']:
+        #     f.write(i)
+        #     if i in ['L/D', 'Re', 'V']:
+        #         a = [mag(np.mean(s(i))) for s in soltab]
+        #     elif i in ['t_s']:
+        #         a = [mag(np.sum(s(i))) for s in soltab]
+        #     else:
+        #         a = [mag(s(i))  for s in soltab]
+        #     for j in range(len(a)):
+        #         if a[j] <= 1e-5:
+        #             a[j] = 0.
+        #     f.write(''.join([" & " + str(np.format_float_scientific(j, precision=2)) for j in a]))
+        #     f.write('\n')
+        # f.write('cost ')
+        # f.write(' '.join(["& " + str(np.format_float_scientific(i['cost'], precision=2)) for i in soltab]))
+        # f.write('\n')
+        # f.close()
 
         self.assertEqual(open(origfilename, 'r').readlines(), open(filename, 'r').readlines())
 
